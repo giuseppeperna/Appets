364e1bd295eae89be611a4e4023eb9a9cc349321
giuseppeperna <giuseppeperna.cg@gmail.com>
2021-03-13 14:22:12 +0100

    New hamburger dropdown

--- resources/views/templates/_header.blade.php
+++ resources/views/templates/_header.blade.php
@@ -4,6 +4,8 @@
       <div class="logo me-auto">
          <h1><a href="{{route('home')}}">Appets</a></h1>
       </div>
+
+      {{-- Navbar con links Desktop  --}}
       <nav id="navbar" class="navbar order-last order-lg-0" :class="{'navbar-mobile': showMenu}">
          <ul>
             <li><a class="nav-link scrollto" href="{{route('home')}}/#restaurants">Ristoranti</a></li>
@@ -23,9 +25,32 @@
             <li><a class="nav-link" href="{{route('accesso')}}">Area ristoranti</a></li>
             @endif
             <li><a class="nav-link" href="{{route('cart.index')}}">Carrello<i class="fas fa-shopping-cart"></i></a></li>
-         </ul>
-         <i class="bi bi-list mobile-nav-toggle" @click="showMenuMobile()" :class="{'bi-list': !showMenu, 'bi-toggle': showMenu}"></i>
+         </ul> 
       </nav>
+
+      {{-- Navbar dropdown con links Mobile --}}
+      <div class="dropdown hamburger-dropdown">
+         <i class="bi bi-list mobile-nav-toggle" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></i>
+         <ul class="dropdown-menu hamburger-dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li><a class="nav-link scrollto" href="{{route('home')}}/#restaurants">Ristoranti</a></li>
+            <li><a class="nav-link scrollto" href="{{route('home')}}/#jobs">Lavora con Appets</a></li>
+            @if(Auth::check())
+            <li><a class="nav-link" href="{{route('dashboard')}}">Dashboard</a></li>
+            <li><a class="nav-link scrollto" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+             {{ __('Logout') }}
+            </a></li>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+            @else
+            <li><a class="nav-link" href="{{route('accesso')}}">Area ristoranti</a></li>
+            @endif
+            <li><a class="nav-link" href="{{route('cart.index')}}">Carrello<i class="fas fa-shopping-cart"></i></a></li>
+         </ul>
+       </div>
       <!-- .navbar -->
    </div>
 </header>
